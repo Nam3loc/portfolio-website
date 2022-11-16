517bc392d5c7dd20767e9fb372342fd972ae1155
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-11-15 20:21:55 -0500

    newest version

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const frontPageName = document.querySelector('#name-color');
 const jobTitle = document.querySelector('#title-color');
-
+const frontPageLinks = document.querySelector('.link');
 
 frontPageName.addEventListener('click', (evt) => {
     if(evt.target){
         evt.target.classList.add("animate__animated", "animate__rubberBand");
     }
-    setTimeout(function(){evt.target.classList.remove("animate__rubberBand") }, 500);
+    setTimeout(function(){evt.target.classList.remove("animate__rubberBand") }, 800);
 })
 
 jobTitle.addEventListener('click', (evt) => {
@@ -14,4 +14,8 @@ jobTitle.addEventListener('click', (evt) => {
         evt.target.classList.add("animate__animated", "animate__backInLeft");
     }
     setTimeout(function(){evt.target.classList.remove("animate__backInLeft") }, 1000);
+})
+
+frontPageLinks.addEventListener('hover', (evt) => {
+        evt.target.textContent = (`</ {evt.target} >`)
 })
